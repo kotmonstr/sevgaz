b3a6442af39e5ca5a519c591cbf0d29959f31cee
Konstantin Sychyov <ksychyov@alvioneurope.ru>
2019-07-10 10:16:49 +0300

    werg

--- wp-content/themes/rambo/template-faq.php
+++ wp-content/themes/rambo/template-faq.php
@@ -18,40 +18,37 @@ if(isset($_GET['post_id'])){
 ?>
 
 <?php my_get_template_part('redline', array('title' => 'FAQ')); ?>
-
     <div class="portfolio_main_content">
-<div class="container"  style="background-color:#f6f5f5 ">
+        <div class="container"  style="background-color:#f6f5f5 ">
 
-
-    <div class="row" style="margin-bottom: 25px">
-
-         <div class="span4">
-             <div class="menu-project">
-                 <ul class="list-menu-project">
-                     <?php while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
-                        <?php $i++ ?>
-                         <a href="/faq/?post_id=<?php echo get_the_ID() ?>"><li class="main-menu <?php echo isset($ID) && $ID == get_the_ID() || $i == 1 && !isset($ID) ? 'active' : ''  ?>"><?php the_title(); ?></li></a>
-                     <?php endwhile; ?>
-                 </ul>
-            </div>
-         </div>
-
-        <div class="span8">
-            <div class="content-project">
-                <?php while ($wp_query_post_first->have_posts()) : $wp_query_post_first->the_post(); ?>
-                    <div class="red-line"></div>
-                    <div class="caption-tile-about">
-                        <p class="caption-text"><?php  the_title(); ?></p>
+            <div class="row" style="margin-bottom: 25px">
+                 <div class="span4">
+                     <div class="menu-project">
+                         <ul class="list-menu-project">
+                             <?php while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
+                                <?php $i++ ?>
+                                 <a href="/faq/?post_id=<?php echo get_the_ID() ?>"><li class="main-menu <?php echo isset($ID) && $ID == get_the_ID() || $i == 1 && !isset($ID) ? 'active' : ''  ?>"><?php the_title(); ?></li></a>
+                             <?php endwhile; ?>
+                         </ul>
                     </div>
-                    <?php  echo get_the_post_thumbnail(); ?>
-                    <?php  the_content(); ?>
+                 </div>
 
-                <?php endwhile; ?>
+                <div class="span8">
+                    <div class="content-project">
+                        <?php while ($wp_query_post_first->have_posts()) : $wp_query_post_first->the_post(); ?>
+                            <div class="red-line"></div>
+                            <div class="caption-tile-about">
+                                <p class="caption-text"><?php  the_title(); ?></p>
+                            </div>
+                            <?php  echo get_the_post_thumbnail(); ?>
+                            <?php  the_content(); ?>
+
+                        <?php endwhile; ?>
+                    </div>
+                </div>
+
             </div>
         </div>
-
-    </div>
-</div>
 </div>
 
 
